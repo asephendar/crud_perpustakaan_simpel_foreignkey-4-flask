97e2d247f50abc618d3aff9cd7bf81cdde853e5c
Asep Hendar <4sep.hendar@gmail.com>
2024-03-13 22:30:23 +0700

    Membuat random books method GET

--- app.py
+++ app.py
@@ -128,5 +128,28 @@ def filter_books():
     
     return {'data': json_data}
 
+@app.route('/random', methods=['GET'])
+def random_books():
+    conn = psycopg2.connect(**params)
+    
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM books ORDER BY RANDOM() LIMIT 1")
+    data = cursor.fetchall()
+    
+    conn.close()
+    
+    json_data = []
+
+    for el in data:
+        json_data.append({
+            'title': el[0],
+            'author': el[1],
+            'year': el[2],
+            'total_pages': el[3],
+            'category': el[4]
+        })
+    
+    return {'data': json_data}
+
 if __name__ == '__main__':
     app.run(debug=True)
